0bb7b72f4fc449a23641e048ffe217d8ff44ddaf
dmitryartemiev <dima77261@gmail.com>
2021-02-01 22:51:50 +0200

    t03 is started

---
+++ t03/js/script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+
+
+
+
+class Human {
+  constructor(firstName, lastName, gender, age, calories) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.gender = gender;
+    this.age = age;
+    this.calories = calories;
+  }
+  sleepFor (){
+
+  }
+  feed() {
+
+  }
+}
+
+class Superhero extends Human {
+    super(calories);
+     fly (){
+
+     }
+     fightWithEvil (){
+
+     }
+}
+
+
+
+
