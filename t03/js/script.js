0d81d427e1610389b9696683a0427bbef6cc83c5
dmitryartemiev <dima77261@gmail.com>
2021-02-03 17:50:57 +0200

    t03 is done (class extend)

--- t03/js/script.js
+++ t03/js/script.js
@@ -16,26 +16,11 @@ class Human {
     this.$portrait = document.getElementsByClassName("portrait")[0];
   }
   sleepFor() {
-    this.$speach.textContent = `I'm sleeping now...`;
-    this.$speach.style.color = "red";
+    waiting = true;
+    talk(this, `I'm sleeping now...`, 5000);
     setTimeout(() => {
-      this.$portrait.style.transform = "scale(1.0)";
-      this.$speach.style.color = "black";
-    }, 500);
-    this.$portrait.style.transform = "scale(1.05)";
-    this.$speach.style.color = "red";
-    setTimeout(() => {
-      this.$speach.textContent = `I'm awake now!`;
-      setTimeout(() => {
-        this.$portrait.style.transform = "scale(1.0)";
-        this.$speach.style.color = "black";
-      }, 500);
-      this.$portrait.style.transform = "scale(1.05)";
-      this.$speach.style.color = "red";
+      talk(this, `I'm awake now!`, 3000);
       waiting = false;
-      setTimeout(() => {
-        this.$speach.textContent = `...`;
-      }, 3000);
     }, 5000);
   }
 
@@ -63,8 +48,15 @@ class Human {
       }, 500);
       setTimeout(() => {
         this.$speach.style.color = "red";
-        this.$speach.textContent = "I'm still hungry";
+
         this.calories += 200;
+        if (this.calories >= 500) {
+          hungry = false;
+          this.$speach.textContent = `I'm not hungry now!`;
+        } else {
+          this.$speach.textContent = "I'm still hungry";
+        }
+
         this.$calories.textContent = this.calories;
         this.$calories.style.color = "red";
         setTimeout(() => {
@@ -81,19 +73,101 @@ class Human {
   }
 }
 
-// class Superhero extends Human {
-//   constructor() {
-//     super(calories, gender);
-//   }
-//   fly() {}
-//   fightWithEvil() {}
-// }
-const person = new Human({});
-person.firstName = "Tony";
-person.lastName = "Stark";
-person.gender = "male";
-person.age = 44;
-person.calories = 0;
+class Superhero extends Human {
+  constructor(age, calories, gender, name, laserBeamEnergy, fightDuration) {
+    super(age, calories, gender);
+    this.name = name;
+    this.laserBeamEnergy = laserBeamEnergy;
+    this.fightDuration = fightDuration;
+    
+  }
+  fly() {
+    waiting = true;
+    talk(this, `I'm flying!`, 10000);
+    this.$portrait.src = "assets/images/fly.jpg";
+    this.$portrait.style.transform = "rotate(5deg)";
+    setTimeout(() => {
+      this.$portrait.style.transform = "rotate(-5deg)";
+    }, 500);
+    setTimeout(() => {
+      this.$portrait.style.transform = "rotate(0deg)";
+    }, 1000);
+    setTimeout(() => {
+      talk(this, `I'm landed!`, 2000);
+      this.$portrait.src = "assets/images/iron_man.jpg";
+      this.$portrait.style.transform = "rotate(5deg)";
+      setTimeout(() => {
+        this.$portrait.style.transform = "rotate(0deg)";
+      }, 500);
+      setTimeout(() => {
+        this.$portrait.style.transform = "rotate(0deg)";
+      }, 1000);
+      waiting = false;
+    }, 10000);
+  }
+  fightWithEvil(fightDuration) {
+    fightDuration = fightDuration * 1000;
+    if (
+      typeof fightDuration === "number" &&
+      !Number.isNaN(fightDuration) &&
+      fightDuration
+    ) {
+      waiting = true;
+      talk(this, "Khhhh-chh... Bang-g-g-g... ", fightDuration);
+
+      this.$portrait.src = "assets/images/fight_evil.jpeg";
+
+      setTimeout(() => {
+        talk(this, "Evil is defeated!", 3000);
+        this.$portrait.src = "assets/images/iron_man.jpg";
+        waiting = false;
+      }, fightDuration);
+    } else {
+      waiting = true;
+      setTimeout(() => {
+        waiting = false;
+      }, 3000);
+      talk(this, "I need a number of seconds for my fight!", 3000);
+    }
+  }
+  shiled() {
+    waiting = true;
+    talk(this, "Jarvis, raise shield!", 3000);
+    this.$portrait.src = "assets/images/shield.jpg";
+    this.$portrait.style.transform = "translate(0px, -20px)";
+    setTimeout(() => {
+      this.$portrait.style.transform = "translate(0px, 0px)";
+    }, 500);
+    setTimeout(() => {
+      this.$portrait.src = "assets/images/iron_man.jpg";
+      waiting = false;
+    }, 3000);
+  }
+  laserBeam() {
+    if(this.laserBeamEnergy !== 0){
+      this.laserBeamEnergy = this.laserBeamEnergy - 25;
+      $laserBeamEnergy.textContent = this.laserBeamEnergy + '%';
+    waiting = true;
+    talk(this, "Jarvis, laser beam!", 5000);
+    this.$portrait.src = "assets/images/laser_beam.gif";
+    this.$portrait.style.transform = "translate(20px, 0px)";
+    setTimeout(() => {
+      this.$portrait.style.transform = "translate(-20px, 0px)";
+    }, 500);
+    setTimeout(() => {
+      this.$portrait.style.transform = "translate(0px, 0px)";
+    }, 1000);
+    setTimeout(() => {
+      this.$portrait.src = "assets/images/iron_man.jpg";
+      waiting = false;
+    }, 5000);
+  }else{
+    talk(this, 'I need more energy!', 3000)
+  }
+  }
+}
+
+const person = new Human("Tony", "Stark", "male", 44, 0);
 
 let waiting = false;
 let global;
@@ -109,7 +183,7 @@ let applyCharacters = () => {
 let everyMinuteHunger = () => {
   clearInterval(global);
   global = setInterval(() => {
-    if (person.calories !== 0) {
+    if (person.calories !== 0 && waiting === false) {
       person.calories -= 200;
       person.$calories.textContent = person.calories;
       imHungry();
@@ -119,18 +193,13 @@ let everyMinuteHunger = () => {
 };
 
 let imHungry = () => {
-  waiting = true;
-  person.$calories.style.color = "red";
-  person.$speach.textContent = "I'm hungry...";
-  person.$speach.style.color = "red";
-  setTimeout(() => {
-    person.$calories.style.color = "black";
-    person.$speach.style.color = "black";
-  }, 500);
-  setTimeout(() => {
-    person.$speach.textContent = "...";
-    waiting = false;
-  }, 3000);
+  if (!waiting) {
+    waiting = true;
+    talk(person, `I'm hungry...`, 3000);
+    setTimeout(() => {
+      waiting = false;
+    }, 3000);
+  }
 };
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -161,5 +230,90 @@ $eatButton.onclick = () => {
   if (!waiting) {
     waiting = true;
     person.feed();
+  }
+};
+
+let talk = (author, message, duration) => {
+  author.$speach.textContent = message;
+  author.$speach.style.color = "red";
+  author.$portrait.style.transform = "scale(1.05)";
+  setTimeout(() => {
+    author.$speach.textContent = "...";
+  }, duration);
+  setTimeout(() => {
+    author.$speach.style.color = "black";
+    author.$portrait.style.transform = "scale(1.0)";
+  }, 500);
+};
+
+let $fly = document.getElementsByClassName("fly")[0];
+let $fight = document.getElementsByClassName("fight")[0];
+let $laserBeam = document.getElementsByClassName("laser_beam")[0];
+let $shield = document.getElementsByClassName("shield")[0];
+let $laserBeamEnergy = document.getElementById("laser");
+let fightDuration;
+
+const hero = new Superhero(
+  person.age,
+  person.calories,
+  person.gender,
+  "Iron man",
+  100
+);
+
+let changeDisplay = () => {
+  $eatButton.style.display = "none";
+  $sleepButton.style.display = "none";
+  $turnSuperhero.style.display = "none";
+  $fly.style.display = "block";
+  $fight.style.display = "block";
+  $laserBeam.style.display = "block";
+  $shield.style.display = "block";
+  person.$portrait.src = "assets/images/iron_man.jpg";
+  document.getElementsByClassName("title")[0].textContent = hero.name;
+  document.getElementsByClassName("property")[0].style.display = "none";
+  document.getElementsByClassName("property")[1].style.display = "none";
+  document.getElementsByClassName("property")[2].style.display = "inline";
+  document.getElementById("hero__name").textContent = hero.name;
+  document.getElementsByClassName("laser_energy")[0].style.display = "inline";
+  document.getElementsByClassName('calories_prop')[0].style.display ='none'
+  $laserBeamEnergy.textContent = hero.laserBeamEnergy + "%";
+};
+
+let $turnSuperhero = document.getElementsByClassName("turn")[0];
+$turnSuperhero.onclick = () => {
+  if (!waiting) {
+    if (hungry === false) {
+      talk(person, "Jarvis, all systems GO!", 3000);
+      changeDisplay();
+      clearInterval(global);
+    } else {
+      imHungry();
+    }
+  }
+};
+
+$fly.onclick = () => {
+  if (!waiting) {
+    hero.fly();
+  }
+};
+
+$fight.onclick = () => {
+  if (!waiting) {
+    hero.fightWithEvil(
+      (fightDuration = Number(prompt("Enter fight duration! (seconds)")))
+    );
+  }
+};
+
+$shield.onclick = () => {
+  if (!waiting) {
+    hero.shiled();
+  }
+};
+$laserBeam.onclick = () => {
+  if (!waiting) {
+    hero.laserBeam();
   }
 };
